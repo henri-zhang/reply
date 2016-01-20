8f29bf63664c26c70f67e20ead1afcd049750670
henri-zhang <henri-zhang>
2016-01-19 22:14:53 -0800

    add myExample files under examples

--- examples/myExample.js
+++ examples/myExample.js
@@ -1,22 +1,39 @@
-var reply = require('./../');
+var reply = require ('./../');
 
-var opts = {
-  name: {
-    message: "What's your name?",
-  },
-  home: {
-    message: "Where do you live?",
-    default: "Seattle"
-  },
-  major: {
-      message: "What's your intended major?",
-      default: "Informatics"
-  }
+var options = {
+    name: {
+        message: "What's your name?",
+    },
+    home: {
+        message: "Where do you live?",
+        default: "Seattle"
+    },
+    year: {
+        message: "What year are you?",
+        options: ["Freshman", "Sophomore", "Junior", "Senior"]
+    },
+    if_freshman: {
+        message: "How are you liking college?",
+        depends_on: {
+            year: "Freshman",
+        }
+    },
+    major: {
+        message: "What's your major?",
+        default: "Informatics"
+    }
 }
 
-reply.confirm ("Will you tell me a little more about yourself?", function(err, yes) {  
-  if (!err && yes) {
-    console.log("Let's begin!");
-  } else {
-    console.log("Go study more.");
-  }
+reply.confirm ("Will you tell me a little more about yourself?", function(err, yes) {
+    if (!err && yes) {
+        console.log("Let's begin!");
+    } else {
+        console.log("Hope to talk more later!");
+        return;
+    }
+    
+    reply.get (options, function (err, answer) {
+        if (err) return;
+        console.log("Your information:\n" + answer);
+    });
+});
